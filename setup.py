04744343a2b5e423f126c576ff6615f59ca61442
fanzhao1986 <fan.zhao@reactivemarkets.com>
2020-02-22 23:38:03 +0000

    feat: rex-py initialization

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+""""
+Setup file.
+"""
+
+from setuptools import setup
+
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
---
+++ tests/test_example.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8
+
+
+def func(x, y):
+    return x + y
+
+
+def test_example():
+    assert func(1, 2) == 3
